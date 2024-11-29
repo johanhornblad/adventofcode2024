c54e293e9b95277066ebf4802c15100464af7670
johanhornblad <johan.hornblad@gmail.com>
2024-11-29 16:54:32 +0100

    textFilter class

---
+++ inc/text_filter.h
@@ -0,0 +1,18 @@
+#ifndef TEXT_FILTER_H
+#define TEXT_FILTER_H
+
+#include <vector>
+#include <string>
+
+
+class TextFilter {
+    public:
+        std::vector<int> findNumbers(const std::string& text) const;
+
+    private:
+
+
+};
+
+
+#endif // TEXT_FILTER_H
