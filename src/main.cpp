3db91caad45b76e9b1bd767679596cc3afde1fdf
johanhornblad <johan.hornblad@gmail.com>
2024-12-02 07:01:05 +0100

    day2

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,129 @@ void bubbleSort(std::vector<int>& numbers) {
 }
 
 
+void day2(const FileHandler& fileHandler) {
+  auto lines = fileHandler.readFileLines();
+  auto textFilter = TextFilter{};
+  auto isAscending = false;
+
+  auto sum = 0;
+
+  for(const auto& line : lines) {
+    auto numbers = textFilter.findNumbers(line);
+
+
+
+    for(std::size_t i = 0; i < numbers.size() - 1; i++) {
+        if(numbers[i] > numbers[i+1]) {
+            if(i > 0 && !isAscending){
+                break;
+            }
+            isAscending = true;
+        }
+        else if(numbers[i] < numbers[i+1]) {
+             if(i > 0 && isAscending){
+                break;
+            }
+            isAscending = false;
+        } 
+        else break;
+
+        auto difference = std::abs(numbers[i + 1] - numbers[i]);
+        if(difference >= 1 && difference < 4) {
+            if(i == numbers.size() - 2) {
+                sum ++;
+            }
+        }
+        else {
+            break;
+        }
+    }
+
+  }
+
+  std::cout << "sum: " << sum << std::endl;
+
+
+
+}
+
+bool isOkayToRemove(const std::vector<int>& numbers, int index, int maxIndex) {
+   if(index == 0) return true;
+
+   if(index == maxIndex) return true;
+
+    auto difference = std::abs(numbers[index - 1] - numbers[index + 1] );
+    return difference >= 1 && difference < 4;
+}
+
+void day2b(const FileHandler& fileHandler) {
+  auto lines = fileHandler.readFileLines();
+  auto textFilter = TextFilter{};
+  auto isAscending = false;
+
+  auto sum = 0;
+
+  for(const auto& line : lines) {
+    auto numbers = textFilter.findNumbers(line);
+
+
+    auto removedLevels = 0;
+    for(std::size_t i = 0; i < numbers.size() - 1; i++) {
+        if(i == 0) removedLevels = 0;
+        if(numbers[i] > numbers[i+1]) {
+            if(i > 0 && !isAscending){
+                if(removedLevels == 0) {
+                   removedLevels ++;
+                } else {
+                   break;
+                }
+            }
+            isAscending = true;
+        }
+        else if(numbers[i] < numbers[i+1]) {
+             if(i > 0 && isAscending){
+                 if(removedLevels == 0) {
+                   removedLevels ++;
+                } else {
+                   break;
+                }
+            }
+            isAscending = false;
+        } 
+        else {
+            if(removedLevels == 0 && isOkayToRemove(numbers, i,numbers.size() - 2)) {
+               removedLevels ++;
+            } else {
+               break;
+            }
+        };
+
+        auto difference = std::abs(numbers[i + 1] - numbers[i]);
+        if(difference >= 1 && difference < 4) {
+            if(i == numbers.size() - 2) {
+                sum ++;
+            }
+        }
+        else {
+            if(removedLevels == 0) {
+                removedLevels ++;
+                if(i == numbers.size() - 2) {
+                    sum ++;
+                }
+            } else {
+                  break;
+               }
+        }
+    }
+
+  }
+
+  std::cout << "sum: " << sum << std::endl;
+
+
+
+}
+
 
 void day1(const FileHandler& fileHandler) {
     auto textFilter = TextFilter{};
@@ -92,8 +215,8 @@ void day1b(const FileHandler& fileHandler) {
 
 int main() {
     
-    auto fileHandler = FileHandler{"inputFiles/day1.txt"};
-    day1b(fileHandler);
+    auto fileHandler = FileHandler{"inputFiles/day2.txt"};
+    day2b(fileHandler);
 
 
     return 0;
